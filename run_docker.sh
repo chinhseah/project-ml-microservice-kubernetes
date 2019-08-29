447ddaa8ef36b06cb8c6231c1589fb58c510f026
Chin Seah <chin.h.seah@nasa.gov>
2019-08-29 12:22:58 -0700

    Update Dockerfile run_docker script
    Updated Dockerfile with steps. Updated run_docker script with docker commands.

--- run_docker.sh
+++ run_docker.sh
@@ -4,9 +4,12 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build --tag=mlmicrokuber .
 
 # Step 2: 
 # List docker images
+docker image ls
 
 # Step 3: 
 # Run flask app
+docker run -it --rm --name mlmicrokuber -p 8000:80 mlmicrokuber
