6ec24e22a8c0bf173f10b990c558426bfa2d156a
Chin Seah <chin.h.seah@nasa.gov>
2019-08-29 17:28:14 -0700

    Update run kubernetes script
    Update run kubernetes script to deploy docker image and expose container port to a host. Also output from kubectl logs <pod name>.

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,19 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
-
+export dockerpath='seahchin/mlmicrokuber'
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+echo "Docker ID and Image: $dockerpath"
+kubectl create deployment mlmicrokuber-node --image=$dockerpath
 
 # Step 3:
 # List kubernetes pods
-
+kubectl get pods
 # Step 4:
 # Forward the container port to a host
+kubectl expose deployment mlmicrokuber-node --type=LoadBalancer --port=80
 
+kubectl get services
+
+minikube service mlmicrokuber-node
